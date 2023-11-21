12dca35e85da8652c2dd0e7ecef20fdb6eaf4850
Keith Butler <keithbutler2001@gmail.com>
2023-11-21 00:34:55 +0000

    fixed indentation

--- home/features/xremap/default.nix
+++ home/features/xremap/default.nix
@@ -2,17 +2,19 @@
 
 {
   # https://youtu.be/lyxScRCe6bE?si=0euSc6YtnGXxkPaf
+  # https://github.com/emberian/evdev/blob/1d020f11b283b0648427a2844b6b980f1a268221/src/scancodes.rs#L26-L572
   services.xremap = {
     # enable = true;
     withX11 = true;
+    # withHypr = true;
     yamlConfig = ''
-modmap:
-    - name: main remaps
-        remap:
-            CapsLock:
-            held: CapsLock
-            alone: esc
-            alone_timeout_millis: 150
+      modmap:
+          - name: main remaps
+              remap:
+                  CapsLock:
+                  held: CapsLock
+                  alone: esc
+                  alone_timeout_millis: 150
     '';
   };
 }
