409e5621665c094da76af40106b181f2318b143c
Keith Butler <keithbutler2001@gmail.com>
2024-02-03 21:31:58 +0000

    enabled jack + disabled low latency

--- hosts/nixos/system/services.nix
+++ hosts/nixos/system/services.nix
@@ -1,6 +1,11 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, config, lib, userSettings, ... }:
 
 {
+
+  nix.settings = {
+    substituters = [ "https://nix-gaming.cachix.org" ];
+    trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
+  };
 
   # Enable sound with pipewire.
   sound.enable = true;
@@ -13,14 +18,15 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
+    # no need to redefine it in your config for now)1
     # media-session.enable = true;
+
     lowLatency = {
       # enable this module
-      enable = true;
+      enable = false;
       # defaults (no need to be set unless modified)
       quantum = 64;
       rate = 48000;
@@ -30,16 +36,16 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  programs.xfconf.enable = true;
-  programs.thunar = {
-    enable = true;
-    plugins = with pkgs.xfce; [
-      thunar-archive-plugin
-      thunar-volman
-    ];
-  };
-  services.gvfs.enable = true; # Mount, trash, and other functionalities
-  services.tumbler.enable = true; # Thumbnail support for images
+  # programs.xfconf.enable = true;
+  # programs.thunar = {
+  #   enable = true;
+  #   plugins = with pkgs.xfce; [
+  #     thunar-archive-plugin
+  #     thunar-volman
+  #   ];
+  # };
+  # services.gvfs.enable = true; # Mount, trash, and other functionalities
+  # services.tumbler.enable = true; # Thumbnail support for images
 
   services.envfs.enable = true;
 
