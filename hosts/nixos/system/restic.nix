4a6e54c7640f31ef4fb0314ae3867f019d36b9a9
Keith Butler <keithbutler2001@gmail.com>
2024-02-03 21:32:53 +0000

    restic test code

---
+++ hosts/nixos/system/restic.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+
+  services.restic.backups = {
+    home-backup = {
+      user = "backups";
+      repository = "/run/media/keith/4TB-BACKUP/backups";
+      initialize = true; # initializes the repo, don't set if you want manual control
+      passwordFile = "<path>";
+      timerConfig = {
+        onCalendar = "saturday 23:00";
+      };
+    };
+  };
+
+}
