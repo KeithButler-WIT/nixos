172cecc408e3a2507511a4dc8d6a6285c88379ea
Keith Butler <keithbutler2001@gmail.com>
2024-02-03 21:32:13 +0000

    new user group + ssh key

--- hosts/nixos/system/users.nix
+++ hosts/nixos/system/users.nix
@@ -3,13 +3,16 @@
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${userSettings.username} = {
-    # initialPassword = "1234";
+    initialPassword = "1234";
     isNormalUser = true;
     description = userSettings.name;
-    extraGroups = [ "networkmanager" "wheel" "docker" "plugdev" "audio" "video" "tty" "input" "storage" "scanner" "lp" "kvm" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "plugdev" "audio" "video" "tty" "input" "storage" "scanner" "lp" "kvm" "nixconfig" "dialout" ];
     packages = with pkgs; [
       floorp
       conda
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNXXNh8eWgPtjc7+iiBFuM6mB+C+8m13wD4tHZFPtYm keithbutler2001@gmail.com"
     ];
   };
 }
