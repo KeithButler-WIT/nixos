dab13bec5b931b43aeecaf564cabf7205f505a26
Keith Butler <keithbutler2001@gmail.com>
2024-02-15 01:11:47 +0000

    removed sddm added greeted

--- hosts/nixos/system/displaymanager.nix
+++ hosts/nixos/system/displaymanager.nix
@@ -5,17 +5,27 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-
-
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
+  # Enable tuigreet login manager
+  services.greetd = {
+    enable = true;
+    settings = {
+      initial_session = {
+        # Auto Login
+        command = "startplasma-wayland";
+        user = "keith";
+      };
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --asterisks --user-menu --cmd startplasma-wayland";
+        user = "greeter";
+      };
+    };
+  };
+
   programs.hyprland = {
-    # Install the packages from nixpkgs
     enable = true;
     # Use the flake
     # package = inputs.hyprland.packages."${pkgs.system}".hyprland;
