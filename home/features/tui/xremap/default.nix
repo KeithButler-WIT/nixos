1e3267ba209b8037d509e8212a50e074cd27ff96
Keith Butler <keithbutler2001@gmail.com>
2024-02-25 13:10:22 +0000

    withWlroots

--- home/features/tui/xremap/default.nix
+++ home/features/tui/xremap/default.nix
@@ -9,8 +9,7 @@
   # https://github.com/emberian/evdev/blob/1d020f11b283b0648427a2844b6b980f1a268221/src/scancodes.rs#L26-L572
   services.xremap = {
     # enable = true;
-    withX11 = true;
-    # withHypr = true;
+    withWlroots = true;
     yamlConfig = ''
       modmap:
           - name: main remaps
