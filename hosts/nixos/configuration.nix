3525da0609b9d891217f27897399d13a5ae597a0
Keith Butler <keithbutler2001@gmail.com>
2024-02-25 13:10:43 +0000

    hwclock + logitech settings

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -15,6 +15,12 @@
     inputs.nix-gaming.nixosModules.pipewireLowLatency
   ];
 
+  time.hardwareClockInLocalTime = true;
+
+  # TODO: MOVE
+  hardware.logitech.wireless.enable = true;
+  hardware.logitech.wireless.enableGraphical = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
