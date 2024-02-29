d9979f274a4b48aa9025244aa6e4f29a8390c50c
Keith Butler <keithbutler2001@gmail.com>
2024-02-29 11:34:28 +0000

    update install script

--- install.sh
+++ install.sh
@@ -45,7 +45,6 @@ The following ZFS datasets will be created:
     - zroot/root (mounted at / with blank snapshot)
     - zroot/nix (mounted at /nix)
     - zroot/tmp (mounted at /tmp)
-    - zroot/home (mounted at /home with blank snapshot)
     - zroot/persist (mounted at /persist)
     - zroot/persist/cache (mounted at /persist/cache)
 
@@ -144,11 +143,6 @@ echo "Creating /tmp"
 sudo zfs create -o mountpoint=legacy zroot/tmp
 sudo mount --mkdir -t zfs zroot/tmp /mnt/tmp
 
-echo "Creating /home"
-sudo zfs create -o mountpoint=legacy zroot/home
-sudo zfs snapshot zroot/home@blank
-sudo mount --mkdir -t zfs zroot/home /mnt/home
-
 echo "Creating /cache"
 sudo zfs create -o mountpoint=legacy zroot/cache
 sudo mount --mkdir -t zfs zroot/cache /mnt/cache
@@ -172,9 +166,9 @@ fi
 sudo mount --mkdir -t zfs zroot/persist /mnt/persist
 
 while true; do
-    read -rp "Which host to install? (nixos / vm) " host
+    read -rp "Which host to install? (desktop / framework / xps / vm / vm-amd) " host
     case $host in
-        nixos|vm ) break;;
+        desktop|nixos|vm ) break;;
         * ) echo "Invalid host. Please select a valid host.";;
     esac
 done
