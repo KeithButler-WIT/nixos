22627ceecd4b3e21a4d77be06d163e3382bd96b4
Keith Butler <keithbutler2001@gmail.com>
2024-03-10 18:08:09 +0000

    add: configs

--- home/features/gui/kitty/default.nix
+++ home/features/gui/kitty/default.nix
@@ -5,21 +5,24 @@
   programs.kitty = {
     enable = true;
     font = {
-      name = "jetbrains mono nerd font";
-      package = pkgs.jetbrains-mono;
+      name = "JetBrainsMono Nerd Font";
+      # name = "jetbrains mono nerd font";
+      # package = pkgs.jetbrains-mono;
       size = 12;
     };
     settings = {
+      shell = "fish";
       bold_font = "auto";
       italic_font = "auto";
       bold_italic_font = "auto";
-      mouse_hide_wait = "2.0";
       cursor_shape = "block";
       # url_color = "#0087bd";
       url_style = "dotted";
       confirm_os_window_close = 0;
       background_opacity = "0.95";
-      shell = "fish";
+      scrollback_lines = 10000;
+      enable_audio_bell = false;
+      mouse_hide_wait = 60;
     };
     shellIntegration.enableFishIntegration = true;
     theme = "Catppuccin-Mocha";
