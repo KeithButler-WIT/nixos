9132bf917530c45b356e6e6ffe065110b23b84ae
Keith Butler <keithbutler2001@gmail.com>
2024-03-20 10:28:11 +0000

    add: thunar

--- hosts/nixos/system/services.nix
+++ hosts/nixos/system/services.nix
@@ -35,18 +35,35 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # programs.xfconf.enable = true;
-  # programs.thunar = {
-  #   enable = true;
-  #   plugins = with pkgs.xfce; [
-  #     thunar-archive-plugin
-  #     thunar-volman
-  #   ];
-  # };
-  # services.gvfs.enable = true; # Mount, trash, and other functionalities
-  # services.tumbler.enable = true; # Thumbnail support for images
+  programs.xfconf.enable = true;
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+    ];
+  };
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
+  environment.systemPackages = with pkgs; [
+    xfce.tumbler # images
+    webp-pixbuf-loader # webp
+    poppler # pdf
+    ffmpegthumbnailer # video
+    freetype # font
+    libgsf # odf
+    nufraw-thumbnailer # raw
+    gnome.totem # video/audio
+    evince # pdf
+    gnome-epub-thumbnailer # epub/mobi
+    mcomix # cbr
+    # folderpreview # folders
+    f3d # 3d files
+  ];
 
   services.envfs.enable = true;
+
+  # services.fwupd.enable = true; # Driver updating
 
   # List services that you want to enable:
 
