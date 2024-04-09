e96e38162bc374d18a58b23fad0d0928733ec4cc
Keith Butler <keithbutler2001@gmail.com>
2024-04-09 20:20:17 +0100

    add vesktop

--- home/features/gui/discord/default.nix
+++ home/features/gui/discord/default.nix
@@ -2,25 +2,28 @@
 
 {
 
-    home.packages = with pkgs; [
-      discord-screenaudio
-      # (discord.override {
-      # withVencord = true;
-      # })
-    ];
+  home.packages = with pkgs; [
+    discord-screenaudio
+    # (pkgs.discord.override {
+    #   # remove any overrides that you don't want
+    #   withOpenASAR = true;
+    #   withVencord = true;
+    # })
+    vesktop
+  ];
 
-    xdg.configFile."Vencord/themes/custom.css".text = ''
-      /**
-      * @name Catppuccin Mocha
-      * @author winston#0001
-      * @authorId 505490445468696576
-      * @version 0.2.0
-      * @description 🎮 Soothing pastel theme for Discord
-      * @website https://github.com/catppuccin/discord
-      * @invite r6Mdz5dpFc
-      * **/
+  xdg.configFile."Vencord/themes/custom.css".text = ''
+    /**
+    * @name Catppuccin Mocha
+    * @author winston#0001
+    * @authorId 505490445468696576
+    * @version 0.2.0
+    * @description 🎮 Soothing pastel theme for Discord
+    * @website https://github.com/catppuccin/discord
+    * @invite r6Mdz5dpFc
+    * **/
 
-      @import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css");
-    '';
+    @import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css");
+  '';
 
 }
