a55883c6eb05361f738290af7309589285f3f765
Keith Butler <keithbutler2001@gmail.com>
2024-04-12 13:51:51 +0100

    remove comment

--- hosts/nixos/System/system.nix
+++ hosts/nixos/System/system.nix
@@ -20,8 +20,6 @@
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
-      # substituters = [ "https://cache.nixos.org" "https://tomodachi94.cachix.org" ];
-      # trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "tomodachi94.cachix.org-1:E1WFk+SYPtq3FFO+NvDgsyciIHg8nHxB/z7qNfojxpI=" ];
     };
     gc = {
       automatic = true;
