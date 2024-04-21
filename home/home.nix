43d3e0011d01962be733b7a4ac443d0dc5ea224f
Keith Butler <keithbutler2001@gmail.com>
2024-04-22 00:13:32 +0100

    move default packages

--- home/home.nix
+++ home/home.nix
@@ -68,42 +68,6 @@
     # release notes.
     stateVersion = "23.05"; # Please read the comment before changing.
 
-    # The home.packages option allows you to install Nix packages into your
-    # environment.
-    packages = with pkgs; [
-      # TODO add to direnv in required projects
-
-      nixpkgs-fmt
-      shfmt
-
-      egl-wayland
-      # pkgs.python311
-      (python311.withPackages (ps: with ps; [ types-beautifulsoup4 beautifulsoup4 ]))
-
-      toybox
-
-      # TODO: Move into a flake in required folders
-      # (pkgs.python310.withPackages (ps: with ps; [ pytz numpy types-beautifulsoup4 beautifulsoup4 requests black pyside6 pylint pillow pywlroots pyflakes poetry-core ]))
-
-      fira-code-symbols
-      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" "SourceCodePro" "FantasqueSansMono" "FiraCode" "OpenDyslexic" "JetBrainsMono" "Hack" ]; })
-      corefonts
-      noto-fonts
-
-      v4l-utils
-
-      termusic
-
-      distrobox
-      docker
-
-      # # You can also create simple shell scripts directly inside your
-      # # configuration. For example, this adds a command 'my-hello' to your
-      # # environment:
-      # (pkgs.writeShellScriptBin "my-hello" ''
-      #   echo "Hello, ${config.home.username}!"
-      # '')
-    ];
   };
 
   lf.enable = true;
