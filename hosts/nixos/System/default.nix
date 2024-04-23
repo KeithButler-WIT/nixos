51c94a714e66982db550fa6f570cba452819e8d5
Keith Butler <keithbutler2001@gmail.com>
2024-04-23 20:52:16 +0100

    move displayermanager into default

--- hosts/nixos/System/default.nix
+++ hosts/nixos/System/default.nix
@@ -7,10 +7,21 @@
     ./polkit.nix
     ./packages.nix
     ./users.nix
-    ./displaymanager.nix
     ./services.nix
     ./boot.nix
   ];
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true; # TODO: check if needed
+
+  hyprland.enable = true;
+  # plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 
 
 }
