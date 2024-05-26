467e51800eba30fea5d26ae68a6bce1695f86961
Keith Butler <keithbutler2001@gmail.com>
2024-05-26 19:21:41 +0100

    remove white space

--- modules/home-manager/desktop/hyprland/config.nix
+++ modules/home-manager/desktop/hyprland/config.nix
@@ -70,39 +70,32 @@ lib.mkIf config.modules.desktop.hyprland.enable
         kb_layout = "us";
         numlock_by_default = false;
         follow_mouse = 1;
-
         touchpad = {
           natural_scroll = true;
           tap-to-click = true;
           disable_while_typing = true;
         };
-
         sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
       };
 
       general = {
         # See https://wiki.hyprland.org/Configuring/Variables/ for more
-
         gaps_in = 5;
         gaps_out = 10;
         border_size = 3;
         # "col.active_border" = "rgba (33 ccffee) rgba (8 f00ffee) 45 deg";
         # "col.inactive_border" = "rgba (595959 aa)";
-
         layout = "dwindle";
       };
 
       decoration = {
         # See https://wiki.hyprland.org/Configuring/Variables/ for more
-
         rounding = 10;
-
         blur = {
           enabled = true;
           size = 5;
           passes = 1;
         };
-
         drop_shadow = true;
         shadow_range = 4;
         shadow_render_power = 3;
@@ -111,11 +104,8 @@ lib.mkIf config.modules.desktop.hyprland.enable
 
       animations = {
         enabled = true;
-
         # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-
         animation = [
           "windows, 1, 7, myBezier"
           "windowsOut, 1, 7, default, popin 80%"
@@ -124,18 +114,15 @@ lib.mkIf config.modules.desktop.hyprland.enable
           "workspaces, 1, 6, default"
         ];
       };
-
       dwindle = {
         # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
         pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
         preserve_split = true; # you probably want this
       };
-
       master = {
         # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
         new_is_master = true;
       };
-
       gestures = {
         # See https://wiki.hyprland.org/Configuring/Variables/ for more
         workspace_swipe = "on";
