03997210928266636edadbe5f4e2c91dd8cc7e6e
Keith Butler <keithbutler2001@gmail.com>
2024-05-30 15:58:12 +0100

    remove qtile section

--- modules/home-manager/shell/fish.nix
+++ modules/home-manager/shell/fish.nix
@@ -31,11 +31,6 @@ in {
         set VIRTUAL_ENV_DISABLE_PROMPT "1"
         set -x MANPAGER "sh -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'"
 
-        ## Export variable need for qt-theme
-        if type "qtile" >> /dev/null 2>&1
-              set -x QT_QPA_PLATFORMTHEME "qt5ct"
-        end
-
         # Set settings for https://github.com/franciscolourenco/done
         set -U __done_min_cmd_duration 10000
         set -U __done_notification_urgency_level low
