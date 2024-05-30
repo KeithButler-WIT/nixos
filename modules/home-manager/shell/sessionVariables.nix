f21d1020c79ab0a0380ac89fc3d837c5077a9593
Keith Butler <keithbutler2001@gmail.com>
2024-05-30 16:03:50 +0100

    remove commented out sessions variables

--- modules/home-manager/shell/sessionVariables.nix
+++ modules/home-manager/shell/sessionVariables.nix
@@ -37,10 +37,13 @@
     __GL_VRR_ALLOWED = "0";
     _JAVA_AWT_WM_NONEREPARENTING = "1";
     SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
+    DISABLE_QT5_COMPAT = "0";
     GDK_BACKEND = "wayland";
     ANKI_WAYLAND = "1";
     DIRENV_LOG_FORMAT = "";
     WLR_DRM_NO_ATOMIC = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     MOZ_ENABLE_WAYLAND = "1";
     WLR_BACKEND = "vulkan";
     # WLR_RENDERER = "vulkan";
