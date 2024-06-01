5b654937bd45862b74ef0036f6c31453e226069f
Keith Butler <keithbutler2001@gmail.com>
2024-06-01 13:44:20 +0100

    add cachix substituters

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -33,6 +33,7 @@
         "https://devenv.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://hyprland.cachix.org"
+        "https://ghostty.cachix.org"
       ];
       extra-trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -40,6 +41,7 @@
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
       ];
       # substituters = [ "https://cache.nixos.org" "https://tomodachi94.cachix.org" ];
       # trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "tomodachi94.cachix.org-1:E1WFk+SYPtq3FFO+NvDgsyciIHg8nHxB/z7qNfojxpI=" ];
