294fbf27229fc3dbf365e2467eb258bbe4001699
Keith Butler <keithbutler2001@gmail.com>
2024-06-02 00:11:02 +0100

    replaced docker for podman

--- modules/nixos/services/docker.nix
+++ modules/nixos/services/docker.nix
@@ -5,19 +5,41 @@ with lib.my;
 let cfg = config.modules.services.docker;
 in {
 
-  options.modules.services.docker.enable =
-    mkBoolOpt false;
+  options.modules.services.docker = {
+    enable = mkBoolOpt false;
+  };
 
   config = mkIf cfg.enable {
-    virtualisation.docker.enable = true;
-
     environment.systemPackages = with pkgs; [
       distrobox
-      docker
+      # docker
+      # docker-compose # start group of containers for dev
+      dive # look into docker image layers
+      podman-tui # status of containers in the terminal
+      podman-compose # start group of containers for dev
     ];
 
-    users.users.${userSettings.username}.extraGroups = [ "docker" ];
+    virtualisation.containers.enable = true;
+    virtualisation = {
+      podman = {
+        enable = true;
+        # Create a `docker` alias for podman, to use it as a drop-in replacement
+        dockerCompat = true;
+        # Required for containers under podman-compose to be able to talk to each other.
+        defaultNetwork.settings.dns_enabled = true;
+        autoPrune.enable = true;
 
+      };
+      # docker = {
+      #   enable = true;
+      #   rootless = {
+      #     enable = true;
+      #     setSocketVariable = true;
+      #   };
+      # };
+    };
+
+    users.users.${userSettings.username}.extraGroups = [ "docker" ];
   };
 
 }
