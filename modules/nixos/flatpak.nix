37309d0db58dcf44a2974040647f6a6299aaaa5f
Keith Butler <keithbutler2001@gmail.com>
2024-06-16 13:56:13 +0100

    fix flatpaks missing DISPLAY

--- modules/nixos/flatpak.nix
+++ modules/nixos/flatpak.nix
@@ -14,13 +14,13 @@ in {
 
   config = lib.mkIf cfg.enable {
 
-    systemd.services.flatpak-repo = {
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.flatpak ];
-      script = ''
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-      '';
-    };
+    # systemd.services.flatpak-repo = {
+    #   wantedBy = [ "multi-user.target" ];
+    #   path = [ pkgs.flatpak ];
+    #   script = ''
+    #     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    #   '';
+    # };
     services.flatpak = {
       enable = true;
       packages = [
@@ -43,11 +43,11 @@ in {
       overrides = {
         global = {
           # Force Wayland by default
-          Context.sockets = [ "wayland" "!x11" "!fallback-x11" ];
+          #Context.sockets = [ "wayland" "!x11" "!fallback-x11" ];
 
           Environment = {
             # Fix un-themed cursor in some Wayland apps
-            XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
+            #XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
 
             # Force correct theme for some GTK apps
             GTK_THEME = "Adwaita:dark";
