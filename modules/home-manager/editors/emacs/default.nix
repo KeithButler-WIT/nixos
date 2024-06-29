428c97386459675b2e539a72832af82300a32ced
Keith Butler <keithbutler2001@gmail.com>
2024-06-29 20:58:40 +0100

    add eamcs

--- modules/home-manager/editors/emacs/default.nix
+++ modules/home-manager/editors/emacs/default.nix
@@ -2,73 +2,65 @@
 
 with lib;
 with lib.my;
-let cfg = config.modules.editors.emacs;
-in {
+let
+  cfg = config.modules.editors.emacs;
+  emacs = with pkgs; (emacsPackagesFor
+    # (if config.modules.desktop.type == "wayland"
+    # then emacs-pgtk
+    # else emacs-git)).emacsWithPackages
+    (emacs-pgtk)).emacsWithPackages
+    (epkgs: [ ]);
+in
+{
 
-  options.modules.editors.emacs.enable =
-    mkBoolOpt false;
+  options.modules.editors.emacs = {
+    enable = mkBoolOpt false;
+  };
 
   config = mkIf cfg.enable {
-    programs.emacs = {
-      enable = true;
-      package = pkgs.emacs;
-      # package = doom-emacs;
-      # extraPackages = with pkgs; [
+    home.packages = with pkgs; [
       ## Emacs itself
-      # binutils # native-comp needs 'as', provided by this
-      #   # 28.2 + native-comp
-      #   # ((emacsPackagesFor emacs-unstable).emacsWithPackages
-      #   #   (epkgs: [
-      #   #     epkgs.vterm
-      #   #     epkgs.f
-      #   #     epkgs.s
-      #   #     epkgs.emacsql
-      #   #     epkgs.emacsql-sqlite
-      #   #     epkgs.magit-section
-      #   #     epkgs.magit-filenotify
-      #   #   ]))
+      binutils # native-comp needs 'as', provided by this
+      # HEAD + native-comp
+      emacs
 
-      #   ## Doom dependencies
-      # git
-      #   (ripgrep.override { withPCRE2 = true; })
-      #   gnutls # for TLS connectivity
+      ## Doom dependencies
+      git
+      ripgrep
+      gnutls # for TLS connectivity
 
       ## Optional dependencies
-      # fd # faster projectile indexing
-      # imagemagick # for image-dired
-      #   # (mkIf (config.programs.gnupg.agent.enable)
-      #   #   pinentry_emacs) # in-emacs gnupg prompts
-      # zstd # for undo-fu-session/undo-tree compression
+      fd # faster projectile indexing
+      imagemagick # for image-dired
+      # (mkIf (config.programs.gnupg.agent.enable)
+      #   pinentry-emacs) # in-emacs gnupg prompts
+      pinentry-emacs
+      zstd # for undo-fu-session/undo-tree compression
 
       ## Module dependencies
       # :checkers spell
-      # (aspellWithDicts (ds: with ds; [ en en-computers en-science ]))
+      (aspellWithDicts (ds: with ds; [ en en-computers en-science ]))
       # :tools editorconfig
-      # editorconfig-core-c # per-project style config
+      editorconfig-core-c # per-project style config
       # :tools lookup & :lang org +roam
-      # sqlite
-      #   # :lang latex & :lang org (latex previews)
-      #   texlive.combined.scheme-medium
-      #   fava
+      sqlite
+      # :lang latex & :lang org (latex previews)
+      # texlive.combined.scheme-medium
+      # :lang nix
+      age
+    ];
 
-      # org-roam deps
-      # dash
-      # ];
-    };
-
-    home.file.".config/doom" = {
-      source = ./doom;
-      recursive = true;
-    };
+    # home.file.".config/doom" = {
+    #   source = ./doom;
+    #   recursive = true;
+    # };
 
     # https://nixos.wiki/wiki/Emacs
-    services.emacs = {
-      enable = true;
-      client.enable = true;
-      package = pkgs.emacs;
-    };
-
-    # fonts.packages = [ pkgs.emacs-all-the-icons-fonts ];
+    # services.emacs = {
+    #   enable = true;
+    #   client.enable = true;
+    #   package = pkgs.emacs;
+    # };
 
     # system.userActivationScripts = {
     #   installDoomEmacs = ''
