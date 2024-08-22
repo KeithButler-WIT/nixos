5a35641179ae85a64ebe091b073c9a04038efcfb
Keith Butler <keithbutler2001@gmail.com>
2024-08-22 15:31:15 +0100

    fix xremap

--- modules/home-manager/services/xremap.nix
+++ modules/home-manager/services/xremap.nix
@@ -9,29 +9,26 @@ in {
     mkBoolOpt false;
 
   imports = [
-    # inputs.xremap-flake.homeManagerModules.default
+    inputs.xremap-flake.homeManagerModules.default
   ];
 
   config = mkIf cfg.enable {
-
     # https://youtu.be/lyxScRCe6bE?si=0euSc6YtnGXxkPaf
     # https://github.com/emberian/evdev/blob/1d020f11b283b0648427a2844b6b980f1a268221/src/scancodes.rs#L26-L572
-    # services.xremap = {
-    #   enabled = true;
-    #   withWlroots = true;
-    #   serviceMode = "user";
-    #   userName = userSettings.username;
-    #   watch = true;
-    #   yamlConfig = ''
-    #     modmap:
-    #         - name: main remaps
-    #             remap:
-    #                 CapsLock:
-    #                 held: CapsLock
-    #                 alone: esc
-    #                 alone_timeout_millis: 150
-    #   '';
-    # };
+    services.xremap = {
+      enable = true;
+      withWlroots = true;
+      watch = true;
+      yamlConfig = ''
+        modmap:
+            - name: main remaps
+                remap:
+                    CapsLock:
+                    held: CapsLock
+                    alone: esc
+                    alone_timeout_millis: 150
+      '';
+    };
   };
 
 }
