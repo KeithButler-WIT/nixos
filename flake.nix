5597450d1291b7dc133eb5d3a01ae73188aa8b76
Keith Butler <keithbutler2001@gmail.com>
2024-11-03 22:09:17 +0000

    change git+https to github

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
 
     # hyprland.url = "github:hyprwm/Hyprland";
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland.url = "github://github.com/hyprwm/Hyprland?submodules=1";
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
