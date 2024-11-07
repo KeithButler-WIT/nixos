448c81c1031408aef8bccfbd06a2bf9cce20eb63
Keith Butler <keithbutler2001@gmail.com>
2024-11-07 16:47:54 +0000

    move packages to out of home to nix

--- hosts/common/packages.nix
+++ hosts/common/packages.nix
@@ -6,6 +6,45 @@
     git
     nixd
     nixfmt-rfc-style
+    nixpkgs-fmt
+    shfmt
+
+    cachix
+    curl
+    nixpkgs-fmt
+    bind
+    cached-nix-shell
+    git
+    vim
+    wget
+    gnumake
+    zip
+    unzip
+    coreutils-full
+    egl-wayland
+    gcc
+    grc
+    ripgrep
+    progress
+    gnupg
+    numlockx
+    light
+
+    # System management
+    htop
+    btop
+    w3m
+    entr
+    killall
+    pass
+    feh
+    gnupg
+    trash-cli
+    ncdu # disk space management
+    xorg.xhost # needed to run gparted on wayland
+    rsync
+    rclone
+    unrar
   ];
 
   nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
