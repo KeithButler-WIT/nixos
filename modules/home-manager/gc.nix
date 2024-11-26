7d47e7313c7c95c1319806a9fa3f315f4321de66
Keith Butler <keithbutler2001@gmail.com>
2024-11-26 14:56:26 +0000

    more gc

---
+++ modules/home-manager/gc.nix
@@ -0,0 +1,24 @@
+{ pkgs, config, lib, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.gc;
+in {
+
+  options.modules.gc.enable =
+    mkBoolOpt false;
+
+  config = mkIf cfg.enable {
+    nix = {
+      settings.auto-optimise-store = true;
+      gc = {
+        automatic = true;
+        persistent = true;
+        frequency = "weekly";
+        randomizedDelaySec = "45min"; 
+        #options = "--max-freed $((64 * 1024**3)) --delete-older-than 7d";
+      };
+    };
+  };
+
+}
