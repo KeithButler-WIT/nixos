0effa6d947adda6d721202c0f554eacda830e384
Keith Butler <keithbutler2001@gmail.com>
2024-12-10 23:10:33 +0000

    add freetube module

---
+++ modules/home-manager/desktop/apps/freetube.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.apps.freetube;
+in {
+
+  options.modules.desktop.apps.freetube.enable =
+    mkBoolOpt false;
+
+  config = mkIf cfg.enable {
+    programs.freetube = {
+      enable = true;
+      settings = {
+        allowDashAv1Formats = true;
+        checkForUpdates     = false;
+        defaultQuality      = "1080";
+        baseTheme           = "catppuccinMocha";
+      };
+    };
+  };
+
+}
