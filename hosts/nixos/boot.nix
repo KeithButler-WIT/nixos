c873f8de6f196b99d5f376e104579eede3a22b42
Keith Butler <keithbutler2001@gmail.com>
2024-12-10 23:13:01 +0000

    remove luks

--- hosts/nixos/boot.nix
+++ hosts/nixos/boot.nix
@@ -5,8 +5,9 @@
   # boot.kernel.sysctl = { "vm.max_map_count" = 2147483642; }; # Not needed while steam.platformOptimizations is enabled
 
   # boot.zfs.forceImportRoot = false;
-  boot.initrd.luks.devices."luks-ee0c8b1c-e042-492a-960d-df1fed98ec91".device = "/dev/disk/by-uuid/ee0c8b1c-e042-492a-960d-df1fed98ec91";
-  networking.hostId = "cb52f555";
+  # boot.initrd.luks.devices."luks-ee0c8b1c-e042-492a-960d-df1fed98ec91".device = "/dev/disk/by-uuid/ee0c8b1c-e042-492a-960d-df1fed98ec91";
+  #networking.hostId = "cb52f555";
+  networking.hostId = "437529cb";
 
   # Refuse ICMP echo requests on my desktop/laptop; nobody has any business
   # boot.kernel.sysctl."net.ipv4.icmp_echo_ignore_broadcasts" = 1;
