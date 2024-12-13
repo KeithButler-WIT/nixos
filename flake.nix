653ba490d952b0dd46b8d27dc1e3912eeff3d3df
Keith Butler <keithbutler2001@gmail.com>
2024-12-13 13:01:39 +0000

    update stable flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Keith's dotfiles managed via NixOS and home-manager";
 
   inputs = {
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
