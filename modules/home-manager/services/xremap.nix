71664a3a4d05d8ee6bc8f737794d34aea5335bda
Keith Butler <keithbutler2001@gmail.com>
2024-12-16 20:53:37 +0000

    withhypr option added

--- modules/home-manager/services/xremap.nix
+++ modules/home-manager/services/xremap.nix
@@ -14,10 +14,9 @@ in {
 
   config = mkIf cfg.enable {
     # https://youtu.be/lyxScRCe6bE?si=0euSc6YtnGXxkPaf
-    # https://github.com/emberian/evdev/blob/1d020f11b283b0648427a2844b6b980f1a268221/src/scancodes.rs#L26-L572
     services.xremap = {
       enable = true;
-      withWlroots = true;
+      withHypr = true;
       watch = true;
       yamlConfig = ''
         modmap:
