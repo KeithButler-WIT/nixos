68daa27a64e532731673e6aab815c95b633b6ded
Keith Butler <keithbutler2001@gmail.com>
2024-12-26 13:24:15 +0000

    add keyd module

---
+++ modules/nixos/services/keyd.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, userSettings, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.services.keyd;
+in {
+
+  options.modules.services.keyd.enable =
+    mkBoolOpt false;
+
+  config = mkIf cfg.enable {
+    users.users.${userSettings.username}.extraGroups = [ "keyd" ];
+    services.keyd = {
+      enable = true;
+      keyboards = {
+        default = {
+          ids = ["*"];
+          settings = {
+            main = {
+              capslock = "overload(esc, capslock)";
+              esc = "overload(esc, capslock)";
+            };
+          };
+        };
+      };
+    }; 
+  };
+
+}
