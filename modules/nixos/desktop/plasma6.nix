b54682a4ee70a91e228e85ff4aaabe4c8846fcc7
Keith Butler <keithbutler2001@gmail.com>
2025-01-10 01:11:22 +0000

    reformat

--- modules/nixos/desktop/plasma6.nix
+++ modules/nixos/desktop/plasma6.nix
@@ -5,21 +5,20 @@
   ...
 }:
 with lib;
-with lib.my;
-let
+with lib.my; let
   cfg = config.modules.desktop.plasma6;
-in
-{
+in {
   options.modules.desktop.plasma6.enable = mkBoolOpt false;
 
   config = lib.mkIf cfg.enable {
-    # Enable the X11 windowing system.
-    services.xserver.enable = true;
-    # Enable the KDE Plasma Desktop Environment.
-    services.xserver.displayManager.sddm.enable = true;
-    services.desktopManager.plasma6.enable = true;
-    environment.plasma6.excludePackages =
-      with pkgs.kdePackages;
+    services = {
+      xserver = {
+        enable = true;
+        displayManager.sddm.enable = true;
+      };
+      desktopManager.plasma6.enable = true;
+    };
+    environment.plasma6.excludePackages = with pkgs.kdePackages;
       [
         elisa
         kate
