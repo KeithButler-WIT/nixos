8960ce1a2c819673c7fd2b59985ae52382a9b8aa
Keith Butler <keithbutler2001@gmail.com>
2025-06-29 12:40:27 +0100

    undo ; between env vars

--- modules/home-manager/desktop/hypr/hyprland.nix
+++ modules/home-manager/desktop/hypr/hyprland.nix
@@ -232,12 +232,12 @@ in {
           "XDG_SESSION_TYPE,wayland"
           "XDG_SESSION_DESKTOP,Hyprland"
           "QT_AUTO_SCREEN_SCALE_FACTOR,1"
-          "QT_QPA_PLATFORM,wayland;xcb"
+          "QT_QPA_PLATFORM,wayland,xcb"
           "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
           "QT_QPA_PLATFORMTHEME,qt5ct"
-          "GDK_BACKEND,wayland;x11;*"
-          "QT_QPA_PLATFORM,wayland;xcb"
-          "SDL_VIDEODRIVER,wayland;x11"
+          "GDK_BACKEND,wayland,x11,*"
+          "QT_QPA_PLATFORM,wayland,xcb"
+          "SDL_VIDEODRIVER,wayland,x11"
           "CLUTTER_BACKEND,wayland"
         ];
         bind = [
