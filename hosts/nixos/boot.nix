938ff0c016219201e675fb8b06cbead16e0840c8
Keith Butler <keithbutler2001@gmail.com>
2025-07-30 11:53:36 +0100

    add both external zfs drives

--- hosts/nixos/boot.nix
+++ hosts/nixos/boot.nix
@@ -10,7 +10,7 @@
   # boot.kernel.sysctl."net.ipv4.icmp_echo_ignore_broadcasts" = 1;
 
   # boot.zfs.enabled = true;
-  # boot.zfs.extraPools = ["zfs-1TB-BACKUP"];
+  boot.zfs.extraPools = ["media" "backup"];
 
   # fileSystems."/run/media/${userSettings.username}/1TB-BACKUP" = {
   #   device = "zfs-1TB-BACKUP/fs1";
@@ -53,18 +53,17 @@
         "x-gvfs-show" # Shows in file managers
       ];
     };
-    # fileSystems."/mnt/4TB-BACKUP" = {
-    "/run/media/${userSettings.username}/4TB-BACKUP" = {
-      # device = "game-drive/fs1";
-      device = "/dev/disk/by-label/4TB-BACKUP";
-      fsType = "exfat";
-      options = [
-        # If you don't have this options attribute, it'll default to "defaults"
-        # boot options for fstab. Search up fstab mount options you can use
-        "users" # Allows any user to mount and unmount
-        "nofail" # Prevent system from failing if this drive doesn't mount
-        "x-gvfs-show" # Shows in file managers
-      ];
-    };
+    # "/run/media/${userSettings.username}/4TB-BACKUP" = {
+    #   # device = "game-drive/fs1";
+    #   device = "/dev/disk/by-label/4TB-BACKUP";
+    #   fsType = "exfat";
+    #   options = [
+    #     # If you don't have this options attribute, it'll default to "defaults"
+    #     # boot options for fstab. Search up fstab mount options you can use
+    #     "users" # Allows any user to mount and unmount
+    #     "nofail" # Prevent system from failing if this drive doesn't mount
+    #     "x-gvfs-show" # Shows in file managers
+    #   ];
+    # };
   };
 }
