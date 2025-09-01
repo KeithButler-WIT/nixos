ab8599856121a78b77a4df1d2e526185f6f87d10
Keith Butler <keithbutler2001@gmail.com>
2025-09-01 15:01:45 +0100

    add lact

--- modules/nixos/hardware/amd.nix
+++ modules/nixos/hardware/amd.nix
@@ -36,5 +36,8 @@ in {
         };
       };
     };
+    environment.systemPackages = with pkgs; [lact];
+    systemd.packages = with pkgs; [lact];
+    systemd.services.lactd.wantedBy = ["multi-user.target"];
   };
 }
