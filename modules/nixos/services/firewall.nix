8c4229040a3e0127d2e163f79897bb17f510c74c
Keith Butler <keithbutler2001@gmail.com>
2025-10-20 20:47:40 +0100

    firewall options

--- modules/nixos/services/firewall.nix
+++ modules/nixos/services/firewall.nix
@@ -4,17 +4,39 @@
   ...
 }:
 with lib;
-with lib.my;
-let
+with lib.my; let
   cfg = config.modules.services.firewall;
-in
-{
+in {
   options.modules.services.firewall.enable = mkBoolOpt false;
 
   config = lib.mkIf cfg.enable {
     # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
+    networking.firewall.allowedTCPPorts = [27036 42805];
+    networking.firewall.allowedTCPPortRanges = [
+      {
+        from = 27015;
+        to = 27016;
+      }
+      {
+        from = 42852;
+        to = 42872;
+      }
+    ];
+    networking.firewall.allowedUDPPorts = [42805];
+    networking.firewall.allowedUDPPortRanges = [
+      {
+        from = 27015;
+        to = 27016;
+      }
+      {
+        from = 27031;
+        to = 27036;
+      }
+      {
+        from = 42852;
+        to = 42872;
+      }
+    ];
     networking.firewall.enable = true;
   };
 }
