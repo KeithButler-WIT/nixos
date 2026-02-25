e5bf36775c16fa5b314a9535ea65445132e34c1a
Keith Butler <keithbutler2001@gmail.com>
2026-02-25 13:51:11 +0000

    zen xdg mimeapps

--- modules/home-manager/desktop/browsers/zen.nix
+++ modules/home-manager/desktop/browsers/zen.nix
@@ -6,14 +6,54 @@
   ...
 }:
 with lib;
-with lib.my; let
+with lib.my;
+let
   cfg = config.modules.desktop.browsers.zen;
-in {
+in
+{
   options.modules.desktop.browsers.zen.enable = mkBoolOpt false;
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       inputs.zen-browser.packages."${system}".default
     ];
+
+    xdg.mimeApps =
+      let
+        value =
+          let
+            zen-browser = inputs.zen-browser.packages.${system}.beta; # or twilight
+          in
+          zen-browser.meta.desktopFileName;
+
+        associations = builtins.listToAttrs (
+          map
+            (name: {
+              inherit name value;
+            })
+            [
+              "application/x-extension-shtml"
+              "application/x-extension-xhtml"
+              "application/x-extension-html"
+              "application/x-extension-xht"
+              "application/x-extension-htm"
+              "x-scheme-handler/unknown"
+              "x-scheme-handler/mailto"
+              "x-scheme-handler/chrome"
+              "x-scheme-handler/about"
+              "x-scheme-handler/https"
+              "x-scheme-handler/http"
+              "application/xhtml+xml"
+              "application/json"
+              "text/plain"
+              "text/html"
+            ]
+        );
+      in
+      {
+        associations.added = associations;
+        defaultApplications = associations;
+      };
+
   };
 }
